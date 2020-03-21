3d561c131f15176c682c2d6e74173f6b2cad72c7
amateur-dev <dipesh.sukhani@gmail.com>
2020-03-22 03:07:09 +0800

    Express REST API with MongoDB and Mongoose Part 2

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
 
 const app = express();
 app.use(bodyParser.json());
-// app.use('/', (req, res) => {
-//   res.status(200).json({ message: 'Connected to the root!' });
-// });;
 app.use('/', routes);
+
+connect.then((db) =>{
+  console.log("Connected correctly to server");
+}, (err) => {
+  console.log(err);
+})
 
 app.listen(3000, () => {
   console.log(`Server is working on localhost:3000`);
