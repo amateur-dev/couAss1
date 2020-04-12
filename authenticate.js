e446ac92df3c9996dcd1b8a0c020ee85e9213f41
amateur-dev <me@dipeshsukhani.dev>
2020-04-12 11:04:06 +0800

    updated node and install esm to work with import

--- authenticate.js
+++ authenticate.js
@@ -1,7 +1,15 @@
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('./models/users');
+const {LocalStrategy , JwtStrategy } = require('passport-local').Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+import * as jsonwebtoken from 'jsonwebtoken';
 
-exports.local = passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser()); // this will handle the sessions of our users
-passport.deserializeUser(User.deserializeUser()); // this will handle the sessions of our users
+
+
+
+// var User = require('./models/users');
+
+// exports.local = passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser()); // this will handle the sessions of our users
+// passport.deserializeUser(User.deserializeUser()); // this will handle the sessions of our users
+
+console.log(jsonwebtoken);
